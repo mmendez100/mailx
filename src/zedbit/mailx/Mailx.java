3128d774075770a383a32f17e5d8a4711f98cb5d
mmendez <mmendez100@gmail.com>
2016-01-23 22:22:54 -0500

    Now it can crawl over dynamic Angular.js nodes

--- src/zedbit/mailx/Mailx.java
+++ src/zedbit/mailx/Mailx.java
@@ -12,6 +12,14 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomText;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+
+
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,12 +34,28 @@ import java.util.regex.Pattern;
  */
 public class Mailx {
 
-    static WebClient webClient;
-    static String startPage;
-    static String uri;
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_RESET = "\u001B[0m";
-    
+    static WebClient webClient; // The headless browser
+    static String startPage; // Where the crawl begins, may be a URI or a page
+    static String uri; // The URI 
+    static Set<String> pagesVisited = new HashSet<String>(); // Tracks pages where we have crawled
+
+	//Prepare to match on regexes for likely emails, very loose, to not miss domains such as .museum :)
+    static final Pattern emailP = Pattern.compile("[\\S^@]+@[\\S^@]+");
+
+    // We will follow only two kinds of links, absolute with our uri, or relative
+    static final Pattern absHrefP = Pattern.compile("(^.*)(href=\")(" + uri +"[^\"]+)(.*$)");
+    static final Pattern relHrefP = Pattern.compile("(^.*)(href=\")(/[^\"]+)(.*$)");
+
+    // Some constants for some pretty-printing 
+    static final String ANSI_RED = "\u001B[31m";
+    static final String ANSI_RESET = "\u001B[0m";
+
+	/**
+ 	* Entry point, a simple scrapping sample program for email extraction.
+ 	*
+ 	* @author Manuel Mendez
+ 	* @since January 22, 2016
+ 	*/
     public static void main(String[] arguments) {
     	if (arguments.length == 0) {
         	System.err.println("Usage: ...Mailx <uri>, i.e. ...Mailx 'http://mysite.com'");
@@ -59,59 +83,68 @@ public class Mailx {
 	        System.out.println("URI: " + uri);
        	 }
 
-
     	// Some setup, turn off superverbose htmlunit logger
        	java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF); 
-		//System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");    
 
-       	// Now begin the drill down!
-        try { 
-        	webClient = new WebClient();
-        	searchEmails(); }
+       	// Get a headless browser
+        try { webClient = new WebClient(BrowserVersion.FIREFOX_38); }
         catch (Exception e) {
+            System.err.println("Could not open browser window! uri=" + uri);
             e.printStackTrace();
         	System.err.println("Exiting...");
 			System.exit(1);
         }
+
+       	// Now begin the drill down!
+		crawl(startPage);
+
     } // end main
 
+	/**
+ 	* Entry point, a simple scrapping sample program for email extraction.
+ 	*
+ 	* @arg pageUrl is an htmlUnit page open (instance of HtmlPage) or a
+ 	*      string representing a page to open.
+ 	* @author Manuel Mendez
+ 	* @since January 22, 2016
+ 	*/
+	public static void crawl(String pageUrl) {
 
-    public static boolean containsPattern(String string, String regex) {
-        Pattern pattern = Pattern.compile(regex);
+  		// First, check if we have been at this URL before, we back out
+  		if (pagesVisited.contains(pageUrl) == true) {
+	        System.out.println("We have already visited " + pageUrl + ". Skipping it");
+  		}
 
-        // Check for the existence of the pattern
-        Matcher matcher = pattern.matcher(string);
-        return matcher.find();
-    } // end containsPattern
-
-
-	public static void searchEmails() throws Exception {
-    	try { 
-
-    		// We create our headless browser
-    		final WebClient webClient = new WebClient();
-
-    	}
-  		catch (Exception e) {
-            System.err.println("Could not open browser window! uri=" + uri);
-            throw(e);
-        }
-
-	    // Suppress myriad of javascript errors, like real browsers do
+ 	    // Suppress myriad of javascript errors, like real browsers do
     	webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setCssEnabled(true); 
+ 		webClient.setCssErrorHandler(new SilentCssErrorHandler()); 
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false); 
+        webClient.getOptions().setThrowExceptionOnScriptError(false); 
+        webClient.getOptions().setRedirectEnabled(false); 
+        webClient.getOptions().setAppletEnabled(false); 
+        webClient.getOptions().setJavaScriptEnabled(true); 
+        webClient.getOptions().setPopupBlockerEnabled(true); 
+        webClient.getOptions().setTimeout(5000); 
+        webClient.getOptions().setPrintContentOnFailingStatusCode(false); 
     		
-    	// Connect and get the page
-        System.out.println("Simulated browser opening " + uri);
-		final HtmlPage page = webClient.getPage(startPage);
+    	// Connect and get the page, backout if we can't.
+        System.out.println("Simulated browser getting page " + pageUrl);
+		HtmlPage page;	// This will be the page we will be working on, as represented by htmlUnit
+		try { page = webClient.getPage(pageUrl); }
+		catch (Exception e) {
+        	System.err.println("Could not get page " + uri);
+        	e.printStackTrace();
+				return;
+		} 
 
 		// Some time for background javascript
-        System.out.println("Simulated browser waiting for background JavaScript completion ");
-    	webClient.waitForBackgroundJavaScript(1 * 1000);
-	   	System.out.println("-------------------------");
+	    System.out.println("Simulated browser waiting for background JavaScript completion ");
+		webClient.waitForBackgroundJavaScript(1 * 1000);
+   		System.out.println("-------------------------");
 
-	  	//Prepare to match on regexes for likely emails, very loose
-       	//as some new domains 
-       	Pattern emailP = Pattern.compile("[\\S^@]+@[\\S^@]+");
+  		// FINALLY The page is open and loaded, now we can search!
 
         //First pass of what we want, any email possibilities that have '@'
        	Set<String> eMails = new HashSet<String>();
@@ -136,11 +169,6 @@ public class Mailx {
         comments.forEach((i) -> {System.out.println(i.toString());});
        	System.out.println("-------------------------");
 
-       	// We will follow only two kinds of links, absolute with our uri, or relative
-       	// If we have been passed a page, we need to get the actual uri
-
-       	Pattern absHref = Pattern.compile("(^.*)(href=\")(" + uri +"[^\"]+)(.*$)");
-      	Pattern relHref = Pattern.compile("(^.*)(href=\")(/[^\"]+)(.*$)");
 
 	    //get list of all anchors that contain either the uri or a local reference
 	    Set<String> localAnchors = new HashSet<String>();
@@ -149,8 +177,8 @@ public class Mailx {
         allAnchors.forEach((i) -> {
         	String iStr = i.toString();
         	System.out.println(iStr);
-        	Matcher m1 = absHref.matcher(iStr);
-        	Matcher m2 = relHref.matcher(iStr);
+        	Matcher m1 = absHrefP.matcher(iStr);
+        	Matcher m2 = relHrefP.matcher(iStr);
         	if (m1.matches()) { localAnchors.add(m1.group(3)); m1.reset(); }
         	if (m2.matches()) { localAnchors.add(m2.group(3)); m2.reset(); }
         });
@@ -166,8 +194,8 @@ public class Mailx {
         allLinks.forEach((i) -> {
         	String iStr = i.toString();
         	System.out.println(iStr);
-        	Matcher m1 = absHref.matcher(iStr);
-        	Matcher m2 = relHref.matcher(iStr);
+        	Matcher m1 = absHrefP.matcher(iStr);
+        	Matcher m2 = relHrefP.matcher(iStr);
         	if (m1.matches()) { localLinks.add(m1.group(3)); m1.reset(); }
         	if (m2.matches()) { localLinks.add(m2.group(3)); m2.reset(); }
         });
@@ -178,13 +206,138 @@ public class Mailx {
 
 
        //get list of all elements that Angular uses for clickable dynamic links
-        final List<?> divs = page.getByXPath("//@ng-click");
-        System.out.println("elements w/ dynamic click hooks: " + divs.size());
-        divs.forEach((i) -> {System.out.println(i.toString());});
+       final List<?> clickables = page.getByXPath("//*[contains(@ng-click,'changeRoute')]");
+
+        System.out.println("elements w/ dynamic click hooks: " + clickables.size());
+        clickables.forEach((i) -> {System.out.println(i.toString());});
        	System.out.println("-------------------------");
 
-       	// More logic could be added for buttons, RoR dynamic content, ASP, etc...
+       	/***** Future upgrades! Here more logic could be added for buttons, RoR dynamic content, ASP, etc... *****/
 
-	} // end searchEmails
+       	// Now here we click on the dynamic content, return via "Back" and keep collecting static content
+       clickables.forEach((i) -> {
+       		System.out.println("Traversing the dynamic link " + i.toString());
+       		HtmlPage newPage;
+       		try { newPage = ((DomElement) i).click(); }
+       		catch (Exception e) {
+       			System.err.println("Click action on " + i.toString() + " failed. Skipping it");
+ 	            e.printStackTrace();
+				return;
+       		}
+       		crawl (newPage);
+       	});
+
+
+       	// Now we recurse on the static content that we accumulated
+
+
+
+	} // end crawl
+
+
+
+
+	public static void crawl(HtmlPage page) {
+
+		final String pageUrl = page.getBaseURL().toString(); 
+
+		// Now check if we have been at this URL before, we back out
+		if (pagesVisited.contains(pageUrl) == true) {
+		    System.out.println("We have already visited " + pageUrl + ". Skipping it");
+	    	try { webClient.getWebWindows().get(0).getHistory().back(); }
+	    	catch (Exception e) {
+       			System.err.println("Back button after dynamic link invocation failed.");
+ 	            e.printStackTrace(); 		
+	    	}
+		    return;
+		}
+
+
+		    System.out.println("Here we would explore " + pageUrl + ". NOOP now");
+		    System.out.println(page.toString());
+
+        //First pass of what we want, any email possibilities that have '@'
+       	Set<String> eMails = new HashSet<String>();
+        final List<?> possibles = page.getByXPath("//text()[contains(.,\"@\")]");
+        System.out.println("possibilities found " + possibles.size());
+        possibles.forEach((i) -> {
+        	String iStr = i.toString();
+        	System.out.println(iStr);
+        	Matcher m = emailP.matcher(iStr);
+        	while (m.find()) { 
+        		eMails.add(iStr);
+        		System.out.println(iStr + ANSI_RED + " <<< Likely an Email!" + ANSI_RESET);
+        	}
+        	m.reset();
+        });
+        System.out.println("total unique local or relative emails found " + eMails.size());
+       	System.out.println("-------------------------");
+
+        //get list of all the comments
+        final List<?> comments = page.getByXPath("//comment()[contains(.,\"@\")]");
+        System.out.println("comments found " + comments.size());
+        comments.forEach((i) -> {System.out.println(i.toString());});
+       	System.out.println("-------------------------");
+
+
+	    //get list of all anchors that contain either the uri or a local reference
+	    Set<String> localAnchors = new HashSet<String>();
+        final List<HtmlAnchor> allAnchors = page.getAnchors();
+        System.out.println("total anchors found " + allAnchors.size());
+        allAnchors.forEach((i) -> {
+        	String iStr = i.toString();
+        	System.out.println(iStr);
+        	Matcher m1 = absHrefP.matcher(iStr);
+        	Matcher m2 = relHrefP.matcher(iStr);
+        	if (m1.matches()) { localAnchors.add(m1.group(3)); m1.reset(); }
+        	if (m2.matches()) { localAnchors.add(m2.group(3)); m2.reset(); }
+        });
+       	System.out.println("---- Anchors to Drill Into: -----");
+        localAnchors.forEach((i) -> { System.out.println(i); });
+        System.out.println("---- Total unique local or relative anchors found: " + localAnchors.size());
+       	System.out.println("-------------------------");
+
+	    //get list of all links
+	    Set<String> localLinks = new HashSet<String>();
+        final List<?> allLinks = page.getByXPath("//link");
+        System.out.println("links found " + allLinks.size());
+        allLinks.forEach((i) -> {
+        	String iStr = i.toString();
+        	System.out.println(iStr);
+        	Matcher m1 = absHrefP.matcher(iStr);
+        	Matcher m2 = relHrefP.matcher(iStr);
+        	if (m1.matches()) { localLinks.add(m1.group(3)); m1.reset(); }
+        	if (m2.matches()) { localLinks.add(m2.group(3)); m2.reset(); }
+        });
+        System.out.println("------ Links to Drill Into: -----");
+        localLinks.forEach((i) -> { System.out.println(i); });
+        System.out.println("------ Total unique local or relative links found: " + localLinks.size());
+       	System.out.println("-------------------------");
+
+
+       //get list of all elements that Angular uses for clickable dynamic links
+       final List<?> clickables = page.getByXPath("//*[contains(@ng-click,'changeRoute')]");
+
+        System.out.println("elements w/ dynamic click hooks: " + clickables.size());
+        clickables.forEach((i) -> {System.out.println(i.toString());});
+       	System.out.println("-------------------------");
+
+       	/***** Future upgrades! Here more logic could be added for buttons, RoR dynamic content, ASP, etc... *****/
+
+	} // end crawl
+
+
+
+
+
+    public static boolean containsPattern(String string, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+
+        // Check for the existence of the pattern
+        Matcher matcher = pattern.matcher(string);
+        return matcher.find();
+    } // end containsPattern
+
+
 
 } // end class MailX
